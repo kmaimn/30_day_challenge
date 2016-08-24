b41899fdf9a9f16cac70ec57f815fba4de09041d
kmaimn <kmai.mn@gmail.com>
2016-08-23 20:05:23 -0500

    python lesson 1

---
+++ ex1.py
@@ -0,0 +1,14 @@
+#hashtag comments out;
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+#creating setting the strings to an array named house, you can call a specific one, in a simliar way.
+house=["Hello World!", "Hello Again", "I like typing this.", "This is fun", 'Yay! Prining.' "I'd much rather you 'not'.", 'I "said" do not touch this.']
+
+print(house[0])
+
+#to print in terminal: python ex1.py;
